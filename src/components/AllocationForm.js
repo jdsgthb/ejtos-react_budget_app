0e4aa370a2b1cfd20e1d6b708cbd15dc31bcefcc
jdsgthb <jdsgthb@users.noreply.github.com>
2023-08-20 18:28:10 -0400

    Update 2

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,12 +1,11 @@
 import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext'; 3.
+import { AppContext } from '../context/AppContext';
 const AllocationForm = (props) => {
-    const { dispatch, remaining } = useContext(AppContext); 6.
+    const { dispatch, remaining } = useContext(AppContext);
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
-    const [action, setAction] = useState(''); 10.
+    const [action, setAction] = useState('');
     const submitEvent = () => {
-        12.
         if (cost > remaining) {
             alert("The value cannot exceed remaining funds £" + remaining);
             setCost("");
@@ -50,7 +49,8 @@ const AllocationForm = (props) => {
                     <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
                         <label className="input-group-text" htmlFor="inputGroupSelect02">Allocation</label>
                     </div>
-                    <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}
+                    <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
+
                     <option defaultValue value="Add" name="Add">Add</option>
                     <option value="Reduce" name="Reduce">Reduce</option>
                 </select>
